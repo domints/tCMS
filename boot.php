99ff31ed09384474b3aa507d676e1038fad36cb5
Dominik Szymański <domints@gmail.com>
2017-03-01 23:04:11 +0100

    Remove Smarty

--- boot.php
+++ boot.php
@@ -1,6 +1,5 @@
 <?php
 require_once './config/config.class.php';
-require_once './lib/Autoloader.php';
 require_once './lib/db.class.php';
 require_once './lib/request.class.php';
 require_once './lib/ui.class.php';
